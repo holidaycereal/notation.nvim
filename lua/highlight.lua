396e0a2305ba8eee137b44962c02920bfe4120ef
Alena Weiley <alenaweiley@gmail.com>
2024-12-09 21:30:44 +1100

    revert operator color

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -12,7 +12,7 @@ return {
 	variables = { fg = "fg" },
 	variables_builtin = { fg = "fg", italic = true },
 	functions = { fg = "fg" },
-	operators = {},
+	operators = { fg = "fg" },
 	punctuation = { fg = "fg" },
 	comments = { fg = "grey_5" },
 	annotations = { fg = "red" },
